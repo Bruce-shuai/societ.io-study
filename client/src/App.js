b09547d906da3b3a5cc161849f9f712538745901
帅得乱七八糟 <284970670@qq.com>
2021-11-22 09:36:04 +0800

    客户端的socket.io连接也完成,但会出现跨域问题

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import './css/app.css';
 import Display from './component/Display';
 import Form from './component/Form';
+import {io} from 'socket.io-client';   // 这里的io其实也是一个函数，我们可以通过这个io获得独立的socket
 
 function App() {
   const [messages, setMessages] = useState([]);
+  const socket = io('http://localhost:3004');    // 目的是连接到前面我设置的3004端口上的服务端
   return (
     <div className="app-container">
       <Display messageList={messages}/>
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')(3000)  // 让socket.io 运行在3000端口上
+const io = require('socket.io')(3004)  // 让socket.io 运行在3004端口上
 
 io.on('connection', socket => {
   console.log(socket.id);  // 这里的id是随机的，当有用户连接上socket.io时，就会分配一个id
