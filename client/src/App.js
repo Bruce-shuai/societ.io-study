1f027579ba62ef066e389ea94650a77507fcbf2c
帅得乱七八糟 <284970670@qq.com>
2021-12-01 12:25:14 +0800

    断开和重新连接

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,50 @@ import { v4 as uuidV4 } from 'uuid'
 const id = uuidV4();
 // 我不把这个放在组件上渲染！
 const socket = io('http://localhost:3004', { query: { id }});  // 得到一个独立的socket
+const userSocket = io('http://localhost:3004/user', {auth: {token: 'test'}});
 
 
 function App() {
   const [messages, setMessages] = useState([]);
   const [room, setRoom] = useState();
-  // const [id, setId] = useState()
-  // useEffect(() => {
-  //   setId(uuidV4())
-  // }, [])
 
   function sendMessage(newMessage) {
-    console.log('room', room);
-    socket.emit('send-message', [...messages, newMessage], room)  // 发送数据到server端
-    setMessages(prev => {
-      console.log('messages', [...prev, newMessage]);
-      return [...prev, newMessage]
-    })
+    console.log(newMessage);
+    socket.emit('send-message', newMessage, room)    
+    setMessages(prev => [...prev, newMessage])
   }
 
-  socket.on('receive-message', (message) => {
-    setMessages(message);
-  })
+
+  useEffect(() => {
+    socket.on('receive-message', (receiveMessage) => {
+      setMessages(prev => {
+        return [...prev, receiveMessage]
+      })
+    })
+  }, [])
 
   function sendRoom(newRoom) {
-    socket.emit('join-room', newRoom)
     setRoom(newRoom)
+    socket.emit('join-room', newRoom, (data) => {
+      console.log('callback', data);
+    });     // 要加入群聊的房间号
   }
 
+
+  // 键盘事件
+  useEffect(() => {
+    document.addEventListener("keydown", connect)   // 不要自己在这里写回调，不然 reomve 也只能写成回调(两个函调函数实质不一样)，则达不到remove的效果
+    return () => document.removeEventListener("keydown", connect)
+  })
+
+  function connect(e) {
+    if (e.key === 'c') {
+      socket.connect();
+    }
+    if (e.key === 'd') {
+      socket.disconnect(); 
+    }
+  }
 
   return (
     <div className="app-container">
